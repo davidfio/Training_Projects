a485cb8c05789cd8512479f0426bb9c4a813418a
David <david94lozoo@gmail.com>
2018-01-30 19:17:29 +0100

    - Added Scriptable Objects training project

---
+++ Scriptable Objects/Assets/Scripts/Card.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Cards")]
+public class Card : ScriptableObject
+{
+    public string cardName;
+    public string description;
+
+    public Sprite artwork;
+
+    public byte manaCost;
+    public byte attack;
+    public byte hp;
+
+    public void Print()
+    {
+        Debug.Log(name + ": " + description + "The card costs: " + manaCost);
+    }
+}
---
+++ Scriptable Objects/Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+
+    public Text nameText;
+    public Text descriptionText;
+
+    public Image artworkImage;
+
+    public Text manaText;
+    public Text attackText;
+    public Text hpText;
+
+	private void Start ()
+    {
+        // I can call a method created in the scriptable object
+        //card.Print();
+
+        SetUpCard();
+        
+    }
+
+    private void SetUpCard()
+    {
+        nameText.text = card.cardName;
+        descriptionText.text = card.description;
+
+        artworkImage.sprite = card.artwork;
+        manaText.text = card.manaCost.ToString();
+        attackText.text = card.attack.ToString();
+        hpText.text = card.hp.ToString();
+    }	
+}
